cfe151568760b8914bcd95d21537ec09dff2e80c
Oleg Kan <olegkahn@gmail.com>
2023-01-20 12:35:30 +0900

    fix UserDaoJDBCIml and Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,45 +6,41 @@ import org.hibernate.cfg.Configuration;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Properties;
 
 public class Util {
     private final String userName = "root";
     private final String password = "root";
     private final String connectionUrl = "jdbc:mysql://localhost:3306/USERS";
-    private Statement statement;
+    private Connection connection;
     private SessionFactory sessionFactory;
     public Util(Boolean b) throws ClassNotFoundException, SQLException {
         if (b) {
             myHibernateSessionFactory();
         } else {
-            myJdbcStatement();
+            myJdbcConnection();
         }
     }
-    private Statement myJdbcStatement() throws ClassNotFoundException, SQLException {
+    private void myJdbcConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection connection = DriverManager.getConnection(connectionUrl, userName, password);
-        statement = connection.createStatement();
-        return statement;
+        connection = DriverManager.getConnection(connectionUrl, userName, password);
     }
-    private SessionFactory myHibernateSessionFactory() {
-            Properties prop= new Properties();
-            prop.setProperty("hibernate.connection.url", connectionUrl);
-            prop.setProperty("dialect", "org.hibernate.dialect.MySQLDialect");
-            prop.setProperty("hibernate.connection.username", userName);
-            prop.setProperty("hibernate.connection.password", password);
-            prop.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
-            prop.setProperty("show_sql", "true");
-            prop.setProperty("hibernate.current_session_context_class", "thread");
-            sessionFactory = new Configuration().addProperties(prop)
-                    .addAnnotatedClass(User.class)
-                    .buildSessionFactory();
-        return sessionFactory;
-        }
+    private void myHibernateSessionFactory() {
+        Properties prop= new Properties();
+        prop.setProperty("hibernate.connection.url", connectionUrl);
+        prop.setProperty("dialect", "org.hibernate.dialect.MySQLDialect");
+        prop.setProperty("hibernate.connection.username", userName);
+        prop.setProperty("hibernate.connection.password", password);
+        prop.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
+        prop.setProperty("show_sql", "true");
+        prop.setProperty("hibernate.current_session_context_class", "thread");
+        sessionFactory = new Configuration().addProperties(prop)
+                .addAnnotatedClass(User.class)
+                .buildSessionFactory();
+    }
 
-    public Statement getStatement() {
-        return statement;
+    public Connection getConnection() {
+        return connection;
     }
     public SessionFactory getSessionFactory() {
         return sessionFactory;
